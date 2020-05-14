83663a53d82653083cf2ee0878df5961ebffdbf3
estefanofc <estefano_fc@hotmail.com>
2020-05-13 17:31:54 -0700

    added first come first served schedule algorithm

--- driver.c
+++ driver.c
@@ -16,34 +16,37 @@
 
 #define SIZE    100
 
-int main(int argc, char *argv[])
-{
-    FILE *in;
-    char *temp;
-    char task[SIZE];
+int main(int argc, char *argv[]) {
+  FILE *in;
+  char *temp;
+  char task[SIZE];
 
-    char *name;
-    int priority;
-    int burst;
+  char *name;
+  int priority;
+  int burst;
+  if (argc != 2) {
+    printf("usage: %s <schedule-file>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  in = fopen(argv[1], "r");
 
-    in = fopen(argv[1],"r");
-    
-    while (fgets(task,SIZE,in) != NULL) {
-        temp = strdup(task);
-        name = strsep(&temp,",");
-        priority = atoi(strsep(&temp,","));
-        burst = atoi(strsep(&temp,","));
+  while (fgets(task, SIZE, in) != NULL) {
+    temp = strdup(task);
+    name = strsep(&temp, ",");
+    priority = atoi(strsep(&temp, ","));
+    burst = atoi(strsep(&temp, ","));
 
-        // add the task to the scheduler's list of tasks
-        add(name,priority,burst);
+    // add the task to the scheduler's list of tasks
+    add(name, priority, burst);
 
-        free(temp);
-    }
+    free(temp);
+  }
 
-    fclose(in);
+  fclose(in);
 
-    // invoke the scheduler
-    schedule();
+  // invoke the scheduler
+  schedule();
+  printf("Done\n");
 
-    return 0;
+  return 0;
 }
--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -1,4 +1,26 @@
 //
 // Created by Estefano Felipa on 5/13/20.
 //
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "list.h"
+#include "schedulers.h"
+#include "cpu.h"
+
+struct node *g_head = NULL;
+
+void add(char *name, int priority, int burst) {
+  Task *newTask = malloc(sizeof(Task));
+  newTask->priority = priority;
+  newTask->burst = burst;
+  newTask->name = strdup(name);
+  printf("--> Added: %s\n", name);
+  insert(&g_head, newTask);
+}
+
+void schedule() {
+  traverse(g_head);
+}
